ed604f50d8f5af473f39f34a51315fb9e2af03e1
Italo Maranhão Santana <150749715+ItaloStana@users.noreply.github.com>
2024-11-16 15:29:24 -0300

    Update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,23 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Configuração do CORS
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5500") // O endereço do seu frontend
-              .AllowAnyHeader()  // Permite qualquer cabeçalho (header)
-              .AllowAnyMethod(); // Permite qualquer método HTTP (GET, POST, PUT, DELETE, etc.)
+        policy.WithOrigins("http://localhost:5500") 
+              .AllowAnyHeader()  
+              .AllowAnyMethod(); 
     });
 });
 
-builder.Services.AddControllers();  // Adiciona suporte ao uso de controllers
+builder.Services.AddControllers();  
 
 var app = builder.Build();
 
-// Usar o CORS configurado
-app.UseCors("AllowFrontend"); // Habilita a política CORS chamada "AllowFrontend"
 
-app.MapControllers();  // Mapear as rotas dos controllers
+app.UseCors("AllowFrontend"); 
 
-app.Run();  // Executa a aplicação
+app.MapControllers();  
+
+app.Run();
